4ef83c999e1e64ef25518e5ad4a608566544154e
Sávio de Carvalho Soares <62923486+saviosoaresc@users.noreply.github.com>
2023-09-06 11:57:51 -0300

    Add files via upload

---
+++ Animal/Main.java
@@ -0,0 +1,33 @@
+package Animal;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+//        Animal animal = new Animal("", "");
+
+        while (true) {
+            var line = input();
+//            var args = line.split(" ");
+            write("$" + line);
+
+            if (args[0].equals("init")) {
+//                animal = new Animal(args[1], args[2]);
+            } else if (args[0].equals("grow")) {
+//                animal.envelhecer((int) number(args[1]));
+            } else if (args[0].equals("noise")) {
+//                write(animal.fazerBarulho());
+            } else if (args[0].equals("show")) {
+//                write(animal.toString());
+            } else if (args[0].equals("end")) {
+                break;
+            } else {
+                write("fail: comando invalido");
+            }
+        }
+    }
+    static Scanner scanner = new Scanner(System.in);
+    public static String input()           { return scanner.nextLine();    }
+    public static void write(String value) { System.out.println(value);    }
+    public static double number(String str)   { return Double.parseDouble(str); }
+}
