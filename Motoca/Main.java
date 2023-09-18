4a05431541ec80ac72a86535c7f1fce1ac47361b
Sávio de Carvalho Soares <62923486+saviosoaresc@users.noreply.github.com>
2023-09-18 10:09:01 -0300

    Add files via upload

---
+++ Motoca/Main.java
@@ -0,0 +1,35 @@
+package Motoca;
+import java.util.*;
+
+public class Main {
+    static Motorcycle motoca = new Motorcycle(1);
+    
+    public static void main(String[] args) {
+        while(true) {
+            String line = input();
+            args = line.split(" ");
+            write('$' + line);
+
+            if      (args[0].equals("show"))     { System.out.println(motoca);                         }
+            else if (args[0].equals("init"))     { motoca = new Motorcycle(number(args[1]));           }  
+            else if (args[0].equals("buy"))      { motoca.buy(number(args[1]));                        }
+            else if (args[0].equals("drive"))    { motoca.drive(number(args[1]));                      }
+            else if (args[0].equals("enter"))    { motoca.enter(new Person(args[1], number(args[2]))); }
+            else if (args[0].equals("honk"))     { motoca.honk();                                      }
+            else if (args[0].equals("leave"))    {
+                Person person = motoca.leave();
+                if(person != null) {
+                    System.out.println(person.toString());
+                }else System.out.println("fail: empty motorcycle");
+            }
+            else if (args[0].equals("end"))      { break;                                              }
+            else
+                System.out.println("fail: comando invalido");
+        }
+        scanner.close();
+    }
+    static Scanner scanner = new Scanner(System.in);
+    public static String input()           { return scanner.nextLine();    }
+    public static void write(String value) { System.out.println(value);    }
+    public static int number(String str)   { return Integer.parseInt(str); }
+}
