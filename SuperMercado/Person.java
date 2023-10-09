ac103519b2a3233819abed287f1d8b4184ff4200
Sávio de Carvalho Soares <62923486+saviosoaresc@users.noreply.github.com>
2023-10-09 13:33:53 -0300

    Add files via upload

---
+++ SuperMercado/Person.java
@@ -0,0 +1,22 @@
+package SuperMercado;
+
+public class Person {
+    private String nome;
+
+    public Person(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
